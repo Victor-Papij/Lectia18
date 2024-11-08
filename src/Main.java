3b540cd18a1eb439b7f269e84f29bb1fd150f202
Victor-Papij <papijviktor@gmail.com>
2024-11-08 17:20:58 +0200

    add

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,40 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //Task1
+        LocalDate date = LocalDate.of( 2024, Month.JANUARY, 2);
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("d MMMM yyyy" );
+        String dateFormat = date.format(dateTimeFormatter);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        //Task2shi3
+        LocalTime time = LocalTime.of( 13,20);
+        DateTimeFormatter dateTimeFormatter2 = DateTimeFormatter.ofPattern("HH:mm");
+        String timeFormat = time.format(dateTimeFormatter2);
+        System.out.println(dateFormat +" "+ timeFormat);
+
+        //Task3
+        LocalDateTime dateTime =  LocalDateTime.of(date,time);
+        DateTimeFormatter dateTimeFormatter3 = DateTimeFormatter.ofPattern("d MMMM yyyy HH:mm" );
+        String dateTimeFormat = dateTime.format(dateTimeFormatter3);
+        System.out.println(dateTimeFormat);
+
+        //Task4
+        ZonedDateTime zonedDateTime1 = dateTime.atZone(ZoneId.of("America/Los_Angeles"));
+        String zonedDateTimeF = zonedDateTime1.format(dateTimeFormatter3);
+        System.out.println(zonedDateTimeF);
+
+        //Task5
+        LocalDateTime dateTime2 = dateTime
+                .plusMinutes(1)
+                .plusHours(1)
+                .plusDays(1)
+                .plusMonths(1)
+                .plusYears(1);
+        String dateTime2Format = dateTime2.format(dateTimeFormatter3);
+        System.out.println(dateTime2Format);
+
     }
 }
